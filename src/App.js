70f29c283d4e2ca61933f2bac1680b5d2997f37b
katttanagatari <b3sh1k@gmail.com>
2024-03-26 18:41:57 +0400

    create react app, remove all from front page

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+      <h1>
+       RESIN
+      </h1>
+    </div>
+  );
+}
+
+export default App;
