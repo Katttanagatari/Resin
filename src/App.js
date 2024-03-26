c9f4154df71eb888ec97a97abf85be370f1695e3
katttanagatari <b3sh1k@gmail.com>
2024-03-26 21:54:23 +0400

    main page

--- src/App.js
+++ src/App.js
@@ -2,47 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-      <h1>
-       RESIN
-      </h1>
-    </div>
+    <header className="header">
+      <div className='container'>
+        <a className='logo' href='%'>
+          Resin
+        </a>
+        <nav className='nav'>
+          <ul className='nav__list'>
+            <li className='nav__item'>
+              <a href='$' className='nav__link'>Offers</a>
+            </li>
+            <li className='nav__item'>
+              <a href='$' className='nav__link'>Blog</a>
+            </li>
+            <li className='nav__item'>
+              <a href='$' className='nav__link'>Team</a>
+            </li>
+            <li className='nav__item'>
+              <a href='$' className='nav__link'>Client reviews</a>
+            </li>
+            <li className='nav__item'>
+              <a href='$' className='nav__link'>Contant me</a>
+            </li>
+          </ul>
+        </nav>
+        <button className='btn'>Get started</button>
+      </div>
+    </header>
   );
 }
 
